dcb6eb0e140549c8190ae12e6022e1d1f36f3f5f
Katy Hatch <katyhatch@hotmail.com>
2024-01-21 14:47:57 +0000

    Initial commit plus exercise 8 homework

---
+++ say_my_name.py
@@ -0,0 +1,13 @@
+# Assigning string value Katy to variable named firstname
+firstname = 'Katy'
+# Assigning string value Hatch to variable named firstname
+lastname = 'Hatch'
+# printing two variables together and will be seperated by a space due to the ,
+print(firstname, lastname)
+
+# Assigning the value of variables firstname plus a space plus a lastname to the new variable named fullname
+fullname = firstname + ' ' + lastname
+# printing the variable fullname
+print(fullname)
+# checking the type of value fullname is
+print(type(fullname))
